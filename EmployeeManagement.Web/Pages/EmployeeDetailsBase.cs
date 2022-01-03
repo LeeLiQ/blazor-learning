fa7e5d7901b1aba94bebe8dd273d9450f87f906f
Qian Li <lqustc@gmail.com>
2022-01-02 16:49:20 -0800

    demo event handling

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages
 {
@@ -17,6 +18,29 @@ namespace EmployeeManagement.Web.Pages
         {
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        protected string Coordinates { get; set; }
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"X = {e.ClientX } Y = {e.ClientY}";
+        }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
     }
 }
